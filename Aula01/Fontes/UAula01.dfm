b77e985ee6ff350df835cfbf0ca1eb147e3bbc1e
RaulAdriano <raul.adriano.ramos@gmail.com>
2019-02-25 21:59:57 -0300

    Aula01
    .

---
+++ Aula01/Fontes/UAula01.dfm
@@ -0,0 +1,32 @@
+object FAula01: TFAula01
+  Left = 0
+  Top = 0
+  Caption = 'Primeiro Formulario'
+  ClientHeight = 299
+  ClientWidth = 635
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object BHello: TButton
+    Left = 272
+    Top = 128
+    Width = 75
+    Height = 25
+    Caption = 'OK'
+    TabOrder = 0
+    OnClick = BHelloClick
+  end
+  object ENome: TEdit
+    Left = 248
+    Top = 101
+    Width = 121
+    Height = 21
+    TabOrder = 1
+  end
+end
---
+++ Aula01/Fontes/__recovery/UAula01.pas
@@ -0,0 +1,35 @@
+unit UAula01;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TFAula01 = class(TForm)
+    BHello: TButton;
+    ENome: TEdit;
+    procedure BHelloClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FAula01: TFAula01;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFAula01.BHelloClick(Sender: TObject);
+var
+  v1,v2,total:integer;
+begin
+  total:= v1 + v2;
+  showmessage('O resultado é   ' + IntToStr(total));
+end;
+
+end.
